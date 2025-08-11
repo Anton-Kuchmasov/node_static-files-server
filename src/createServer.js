a7a839445088a19891525ce92ecbff9524ec3d19
Anton Kuchmasov <antek.ukraine@gmail.com>
2025-08-11 16:50:30 +0300

    fixed tests

--- src/createServer.js
+++ src/createServer.js
@@ -3,20 +3,29 @@
 const http = require('http');
 const fs = require('fs/promises');
 const path = require('path');
+const axios = require('axios');
 
 function createServer() {
   const fileServer = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
 
-    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
-
-    if (normalizedURL.pathname.includes('//')) {
+    if (req.url.includes('//')) {
       res.statusCode = 404;
 
-      return res.end('Bad filepath!');
+      return res.end('Double slashes are prohibited!');
     }
 
+    if (req.url.includes('..')) {
+      res.statusCode = 400;
+
+      return res.end('Access denied!');
+    }
+
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
     if (!normalizedURL.pathname.startsWith('/file')) {
+      res.statusCode = 400;
+
       return res.end(
         'Hint: to download a file from public dir, use /file/ prefix',
       );
@@ -26,14 +35,12 @@ function createServer() {
       normalizedURL.pathname.replace('/file', '') || 'index.html';
 
     const finalPath = path.join(__dirname, '../public', filePath);
-    const publicDir = path.join(__dirname, '../public');
+    const publicDir = path.resolve(__dirname, '../public');
 
     if (!finalPath.startsWith(publicDir)) {
       res.statusCode = 400;
 
-      res.end("Don't bullshit me, Carl!");
-
-      return;
+      return res.end('Access denied!');
     }
 
     fs.readFile(finalPath, 'utf8')
@@ -51,6 +58,8 @@ function createServer() {
 
   return fileServer;
 }
+
+axios.get('http://localhost:5701/file/index.html').then(() => {});
 
 module.exports = {
   createServer,
